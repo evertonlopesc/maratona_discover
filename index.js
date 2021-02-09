8b61956f0e06b5815017b34f8a2de7a3d8557f3c
Everton Lopes Costa <everton.lopess@outlook.com>
2021-02-09 07:23:28 -0300

    Alter - formatacao das moedas e calculos de incomes, expenses e total.

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ const transactions = [
   {
     id: 1,
     description: "luz",
-    amount: -50000,
+    amount: -50004,
     date: "04/02/2021",
   },
   {
     id: 2,
     description: "Website",
-    amount: 500000,
+    amount: 500018,
     date: "04/02/2021",
   },
   {
@@ -37,14 +37,36 @@ const transactions = [
 ];
 
 const Transaction = {
-  income() {
-    // somar as entradas
+  all: transactions,
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+  },
+
+  incomes() {
+    let income = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+
+    return income;
   },
   expenses() {
-    // somar as saídas
+    let expense = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    });
+
+    return expense;
   },
   total() {
-    // entradas - saídas
+    return Transaction.incomes() + Transaction.expenses();
   },
 };
 
@@ -65,7 +87,7 @@ const DOM = {
 
     const html = `
       <td class="description">${transaction.description}</td>
-      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
         <img src="./assets/minus.svg" alt="Remover transação" />
@@ -74,14 +96,36 @@ const DOM = {
 
     return html;
   },
+
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+  },
 };
 
 const Utils = {
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : "";
+    value = String(value).replace(/\D/, "");
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
   },
 };
 
 transactions.forEach(function (transaction) {
   DOM.addTransaction(transaction);
 });
+
+DOM.updateBalance();
