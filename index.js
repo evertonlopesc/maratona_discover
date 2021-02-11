1eda12363c3997d06c7fe8a5d03ec6c1508a6d18
Everton Lopes Costa <everton.lopess@outlook.com>
2021-02-11 07:18:31 -0300

    Add - submit tem as funcoes formatValues, saveTransaction, clearFildes.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const Transaction = {
       date: "04/02/2021",
     },
   ],
-
+ 
   add(transaction) {
     Transaction.all.push(transaction);
 
@@ -57,6 +57,7 @@ const Transaction = {
 
     return income;
   },
+
   expenses() {
     let expense = 0;
 
@@ -68,6 +69,7 @@ const Transaction = {
 
     return expense;
   },
+  
   total() {
     return Transaction.incomes() + Transaction.expenses();
   },
@@ -78,12 +80,13 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr);
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
     const amount = Utils.formatCurrency(transaction.amount);
@@ -93,7 +96,7 @@ const DOM = {
       <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-        <img src="./assets/minus.svg" alt="Remover transação" />
+        <img onclick = "Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação" />
       </td>
     `;
 
@@ -118,6 +121,22 @@ const DOM = {
 };
 
 const Utils = {
+  formatAmount(value) {
+    // value = Number(value.replace(/\.\,/g, "")) * 100;
+    value = Number(value) * 100;
+    return value;
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split("-");
+    
+    const day = splittedDate[2];
+    const month = splittedDate[1];
+    const year = splittedDate[0];
+
+    return `${day}/${month}/${year}`;
+  },
+
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : "";
     value = String(value).replace(/\D/, "");
@@ -149,9 +168,31 @@ const Form = {
 
     if (description.trim() === "" || 
         amount.trim() === "" || 
-        date.trim()) {
+        date.trim() === "") {
           throw new Error("Por favor preencha todos os campos.");
     }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date
+    }
+  },
+
+  saveTransaction(transaction) {
+    Transaction.add(transaction);
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
   },
 
   submit(event) {
@@ -159,7 +200,11 @@ const Form = {
 
     try {
       Form.validateFields();
-      
+      const transaction = Form.formatValues();
+      Form.saveTransaction(transaction);
+      Form.clearFields();
+      Modal.verifyOpenClose();
+
     } catch (error) {
       alert(error.message);
     }
@@ -168,9 +213,7 @@ const Form = {
 
 const App = {
   init() {
-    Transaction.all.forEach((transaction) => {
-      DOM.addTransaction(transaction);
-    });
+    Transaction.all.forEach(DOM.addTransaction);
 
     DOM.updateBalance();
   },
